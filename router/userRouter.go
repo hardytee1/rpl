93870562a2d2f784759944ee01d701fc66a68a83
hardytee1 <hardytee29@gmail.com>
2024-04-25 10:29:10 +0700

    feat change:/validate to /me

--- router/userRouter.go
+++ router/userRouter.go
@@ -16,7 +16,7 @@ func UserRouter(router *gin.Engine) {
 
 	userRoutes.POST("/register", controllers.Register)
 	userRoutes.POST("/login", controllers.Login)
-	userRoutes.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	userRoutes.GET("/me", middleware.RequireAuth, controllers.Validate)
 	userRoutes.POST("/logout", middleware.RequireAuth, controllers.Logout)
 	userRoutes.POST("/createacc", controllers.CreateAccount)
 	userRoutes.DELETE("/deleteacc/:id", middleware.RequireAuth, middleware.RequireRole(models.RoleAdmin), controllers.DeleteAccount)
